a280c1be1d7268ef8e452949e61d6b3826a02f5b
Nayte <nayte91@gmail.com>
2022-10-13 15:18:07 +0200

    removed unused show variable

--- src/Services/Hooks/ModalStructure.js
+++ src/Services/Hooks/ModalStructure.js
@@ -1,32 +1,28 @@
-import React, { forwardRef, useState } from 'react'
+import React, { forwardRef } from 'react'
 import ReactDOM from 'react-dom'
 
 const ModalStructure = forwardRef(
     ({ title, children, header = true, anchor = null }, ref) => {
-        const [isShowing, setIsShowing] = useState(true)
         const closeDialog = () => ref.current.close()
 
-        return (
-            isShowing &&
-            ReactDOM.createPortal(
-                <dialog className="modal" ref={ref}>
-                    {header && (
-                        <header className="modal__header">
-                            <button
-                                type="button"
-                                className="modal-close-button"
-                                data-dismiss="modal"
-                                onClick={() => closeDialog()}
-                            >
-                                <span>&times;</span>
-                            </button>
-                            <h1>{title}</h1>
-                        </header>
-                    )}
-                    <main className="modal__main">{children}</main>
-                </dialog>,
-                anchor || document.body
-            )
+        return ReactDOM.createPortal(
+            <dialog className="modal" ref={ref}>
+                {header && (
+                    <header className="modal__header">
+                        <button
+                            type="button"
+                            className="modal-close-button"
+                            data-dismiss="modal"
+                            onClick={() => closeDialog()}
+                        >
+                            <span>&times;</span>
+                        </button>
+                        <h1>{title}</h1>
+                    </header>
+                )}
+                <main className="modal__main">{children}</main>
+            </dialog>,
+            anchor || document.body
         )
     }
 )
